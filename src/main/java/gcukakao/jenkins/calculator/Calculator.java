cc87bca7295847b467821b4a651236307d15f016
SONG YUJIN <j_jongleur@naver.com>
2022-10-20 13:30:43 +0900

    temp

--- src/main/java/gcukakao/jenkins/calculator/Calculator.java
+++ src/main/java/gcukakao/jenkins/calculator/Calculator.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 @Service 
 public class Calculator {
      // 주석 추가
+     ////
      public int sum(int a, int b) { 
           return a + b; 
      } 
