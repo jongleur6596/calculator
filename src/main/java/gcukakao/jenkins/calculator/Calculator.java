fd4b1daa4d94d6f1801fe12eacfa7e588a64bd0c
SONG YUJIN <j_jongleur@naver.com>
2022-10-20 11:49:17 +0900

    add line

--- src/main/java/gcukakao/jenkins/calculator/Calculator.java
+++ src/main/java/gcukakao/jenkins/calculator/Calculator.java
@@ -2,7 +2,8 @@ package gcukakao.jenkins.calculator;
 import org.springframework.stereotype.Service; 
  
 @Service 
-public class Calculator { 
+public class Calculator {
+     // 주석 추가
      public int sum(int a, int b) { 
           return a + b; 
      } 
